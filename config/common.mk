052960e5d35666f607d73198990136ac6d023b33
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-04-16 06:24:34 +0200

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Allow vendor/extra to override any property by setting it first
-$(call inherit-product-if-exists, vendor/extra/product.mk)
+# $(call inherit-product-if-exists, vendor/extra/product.mk)
 
 PRODUCT_BRAND ?= dotOS
 
